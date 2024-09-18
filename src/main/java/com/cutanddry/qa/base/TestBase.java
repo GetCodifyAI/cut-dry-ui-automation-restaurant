6a3695b65af57046ae7863d6ddd78606814c56db
Tharaka Galappaththi <tharakagalappaththi@Tharakas-MacBook-Pro.local>
2024-09-18 12:33:52 +0530

    bvt.xml+wait change

--- src/main/java/com/cutanddry/qa/base/TestBase.java
+++ src/main/java/com/cutanddry/qa/base/TestBase.java
@@ -31,8 +31,8 @@ public class TestBase {
                     }
                     driver = new ChromeDriver(chromeOptions);
                     js = (JavascriptExecutor) driver;
-                    wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
+                    wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
                     driver.get(Constants.MAIN_URL);
                     restaurantUI = new KeywordBase(driver, wait);  // Initialize KeywordBase here
 
