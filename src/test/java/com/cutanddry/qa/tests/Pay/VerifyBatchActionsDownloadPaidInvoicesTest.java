133533c52df8f482fdfbab3cc71d57d074955612
virangabandara <viranga@cutanddry.com>
2024-11-28 11:48:49 +0530

    Add VerifyBatchActionsDownloadPaidInvoicesTest

---
+++ src/test/java/com/cutanddry/qa/tests/Pay/VerifyBatchActionsDownloadPaidInvoicesTest.java
@@ -0,0 +1,47 @@
+package com.cutanddry.qa.tests.Pay;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Pay;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyBatchActionsDownloadPaidInvoicesTest extends TestBase{
+    static User user;
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-602")
+    public void VerifyBatchActionsDownloadPaidInvoices()throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Pay.navigateToPay();
+        softAssert.assertTrue(Pay.isPaySupplierTextDisplayed(),"error in text display");
+        Pay.clickOnPaidInvoiceTab();
+        Pay.clickSelectInvoice();
+        Pay.clickBatchActions();
+        Pay.clickDownloadInvoices();
+        softAssert.assertTrue(Pay.isInvoicesSentPopUpDisplayed(),"Invoice sent to email pop up not display");
+        Pay.clickOk();
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
