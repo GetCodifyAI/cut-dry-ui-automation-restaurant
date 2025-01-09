94205d5e5630989785dd54ed8073cf996637c6e3
virangabandara <viranga@cutanddry.com>
2025-01-09 23:22:26 +0530

    Add VerifyAddNewSupplierFlowWithASingleSupplierTest

---
+++ src/test/java/com/cutanddry/qa/tests/Orders/VerifyAddNewSupplierFlowWithASingleSupplierTest.java
@@ -0,0 +1,60 @@
+package com.cutanddry.qa.tests.Orders;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Order;
+import com.cutanddry.qa.functions.Suppliers;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyAddNewSupplierFlowWithASingleSupplierTest extends TestBase {
+    static User user;
+    static String SupplierName = "TestSupplier";
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-923")
+    public void VerifyAddNewSupplierFlowWithASingleSupplier() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Order.goToOrder();
+        softAssert.assertTrue(Order.isPlaceOrderTextDisplayed(),"Order navigation error");
+        Order.clickAddNewSupplier();
+        softAssert.assertTrue(Suppliers.isAddSuppliersPopUpDisplayed(),"Add supplier pop up window not displayed");
+        Suppliers.clickContinue();
+        Suppliers.enterSupplier(SupplierName);
+        Suppliers.clickContinue();
+        Suppliers.clickSave();
+        Order.goToOrder();
+        softAssert.assertTrue(Order.isPlaceOrderTextDisplayed(),"Order navigation error");
+        softAssert.assertTrue(Order.isAddedNewSupplierDisplayed(SupplierName),"error in supplier creation");
+
+        //delete supplier
+        Suppliers.goToSuppliers();
+        softAssert.assertTrue(Suppliers.isUserNavigatedToSupplier(),"Supplier navigation error");
+        Suppliers.selectOneSupplier(SupplierName);
+        softAssert.assertTrue(Suppliers.isEditSuppliersPopUpDisplayed(),"Edit supplier pop up window not displayed");
+        Suppliers.deleteSupplier();
+
+        Order.goToOrder();
+        softAssert.assertTrue(Order.isPlaceOrderTextDisplayed(),"Order navigation error");
+        softAssert.assertFalse(Order.isAddedNewSupplierDisplayed(SupplierName),"error in supplier creation");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
