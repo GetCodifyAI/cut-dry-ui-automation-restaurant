3e64eb6b411f3d693375332d6540911d85dfc994
virangabandara <viranga@cutanddry.com>
2025-01-09 11:30:59 +0530

    Add VerifyViewArchivedSupplierTest

---
+++ src/test/java/com/cutanddry/qa/tests/Suppliers/VerifyViewArchivedSupplierTest.java
@@ -0,0 +1,65 @@
+package com.cutanddry.qa.tests.Suppliers;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Suppliers;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyViewArchivedSupplierTest extends TestBase {
+    static User user;
+    static String SupplierName = "TestSupplier07";
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-956")
+    public void VerifyViewArchivedSupplier () throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Suppliers.goToSuppliers();
+        softAssert.assertTrue(Suppliers.isUserNavigatedToSupplier(),"Supplier navigation error");
+        //add supplier
+        Suppliers.clickAddSupplier();
+        softAssert.assertTrue(Suppliers.isAddSuppliersPopUpDisplayed(),"Add supplier pop up window not displayed");
+        Suppliers.clickContinue();
+        Suppliers.enterSupplier(SupplierName);
+        Suppliers.clickContinue();
+        Suppliers.clickSave();
+        softAssert.assertTrue(Suppliers.isSupplierDisplayed(SupplierName),"error in supplier creation");
+
+        //Archived supplier
+        Suppliers.selectOneSupplier(SupplierName);
+        softAssert.assertTrue(Suppliers.isEditSuppliersPopUpDisplayed(),"Edit supplier pop up window not displayed");
+        Suppliers.clickOnArchived();
+        Suppliers.clickViewArchived();
+        softAssert.assertTrue(Suppliers.isArchivedSupplierPopUpDisplayed(),"Archived supplier pop up window not displayed");
+        softAssert.assertTrue(Suppliers.isArchivedSupplierDisplayed(),"Archived supplier not displayed");
+        Suppliers.clickUnarchived();
+        Suppliers.clickCloseArchived();
+        softAssert.assertTrue(Suppliers.isSupplierDisplayed(SupplierName),"error in supplier unarchived");
+
+        //Delete supplier
+        Suppliers.selectOneSupplier(SupplierName);
+        softAssert.assertTrue(Suppliers.isEditSuppliersPopUpDisplayed(),"Edit supplier pop up window not displayed");
+        Suppliers.deleteSupplier();
+        softAssert.assertFalse(Suppliers.isSupplierDisplayed(SupplierName),"error in supplier delete");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
