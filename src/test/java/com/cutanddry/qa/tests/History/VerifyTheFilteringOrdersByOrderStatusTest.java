8cd4c7593205898345c2b9cac451087eeaea3f0e
virangabandara <viranga@cutanddry.com>
2025-01-13 11:18:10 +0530

    Add VerifyTheFilteringOrdersByOrderStatusTest

---
+++ src/test/java/com/cutanddry/qa/tests/History/VerifyTheFilteringOrdersByOrderStatusTest.java
@@ -0,0 +1,43 @@
+package com.cutanddry.qa.tests.History;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.History;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyTheFilteringOrdersByOrderStatusTest extends TestBase {
+    static User user;
+    String status = "Submitted";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+    @Test(groups = "DOT-TC-949")
+    public void VerifyTheFilteringOrdersByOrderStatus() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        History.goToHistory();
+        softAssert.assertTrue(History.isUserNavigatedToHistory(),"History navigation error");
+        History.selectOrderStatus(status);
+        softAssert.assertTrue(History.isOrderStatusChanged(status),"dropdown error");
+        softAssert.assertTrue(History.validateOrdersStatus(status),"Error in filtering order status");
+        softAssert.assertAll();
+
+    }
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
