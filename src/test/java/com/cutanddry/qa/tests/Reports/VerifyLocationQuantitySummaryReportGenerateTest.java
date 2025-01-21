dd638a60cb625228da55ea1bdefab51e9355f914
virangabandara <viranga@cutanddry.com>
2025-01-21 16:01:46 +0530

    Add VerifyLocationQuantitySummaryReportGenerateTest

---
+++ src/test/java/com/cutanddry/qa/tests/Reports/VerifyLocationQuantitySummaryReportGenerateTest.java
@@ -0,0 +1,51 @@
+package com.cutanddry.qa.tests.Reports;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Reports;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyLocationQuantitySummaryReportGenerateTest extends TestBase {
+    static User user;
+    static String report = "location quantity summary";
+    static String reportName = "Location Quantity Summary";
+    static String productName = "Artichoke -24CT";
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-948")
+    public void VerifyLocationQuantitySummaryReportGenerate()throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToReports();
+        softAssert.assertTrue(Reports.disabledGenerateReport(),"error in  generate report button");
+        Reports.selectTheReport(report);
+        Reports.generateReport();
+        softAssert.assertTrue(Reports.isGeneratedReportDisplayed(reportName),"error in  generating table");
+        Reports.searchData(productName);
+        softAssert.assertTrue(Reports.isSearchResultDisplayed(productName),"generate report not successfully");
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
