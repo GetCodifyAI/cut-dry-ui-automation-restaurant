7a5b4a986ad8da60ffaa9b8d876bb3a6be6dfe3f
virangabandara <viranga@cutanddry.com>
2025-01-21 16:06:30 +0530

    Add VerifyDailyExpensesReportGenerateTest

---
+++ src/test/java/com/cutanddry/qa/tests/Reports/VerifyDailyExpensesReportGenerateTest.java
@@ -0,0 +1,46 @@
+package com.cutanddry.qa.tests.Reports;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Reports;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyDailyExpensesReportGenerateTest extends TestBase {
+    static User user;
+    static String report = "daily expenses report";
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-952")
+    public void VerifyDailyExpensesReportGenerate()throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToReports();
+        softAssert.assertTrue(Reports.disabledGenerateReport(),"error in  generate report button");
+        Reports.selectTheReport(report);
+        Reports.generateReport();
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
