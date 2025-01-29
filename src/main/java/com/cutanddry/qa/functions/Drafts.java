92b9f296f522862b348d93c75479bf171c3d3835
virangabandara <viranga@cutanddry.com>
2025-01-29 21:54:02 +0530

    add VerifyDeleteDraftOrderTest

--- src/main/java/com/cutanddry/qa/functions/Drafts.java
+++ src/main/java/com/cutanddry/qa/functions/Drafts.java
@@ -20,6 +20,24 @@ public class Drafts {
     public static void clickEditOrder(){
         draftsPage.clickEditOrder();
     }
+    public static String getReferenceNum() throws InterruptedException {
+        return draftsPage.getReferenceNum();
+    }
+    public static void clickTrashIcon()throws InterruptedException{
+        draftsPage.clickTrashIcon();
+    }
+    public static boolean isDraftsDeleteTextDisplayed(){
+        return draftsPage.isDraftsDeleteTextDisplayed();
+    }
+    public static boolean isDraftDeleteConfirmationDisplayed(String text){
+        return draftsPage.isDraftDeleteConfirmationDisplayed(text);
+    }
+    public static void clickYesButton()throws InterruptedException{
+        draftsPage.clickYesButton();
+    }
+    public static boolean isReferenceNumberDisplayed(String number)throws InterruptedException{
+       return draftsPage.isReferenceNumberDisplayed(number);
+    }
 
 
 }
---
+++ src/test/java/com/cutanddry/qa/tests/Drafts/VerifyDeleteDraftOrderTest.java
@@ -0,0 +1,69 @@
+package com.cutanddry.qa.tests.Drafts;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Customer;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Drafts;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyDeleteDraftOrderTest extends TestBase {
+    static User user;
+    static String itemName, searchItemCode,referenceNum;
+    static double itemPrice;
+    static String draftConfirmation ="This draft was created by Mashan. Deleting it will remove it permanently, and it cannot be restored.";
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-936")
+    public void VerifySubmitDraftOrderByAddingItemsFromCatalog() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+
+        Dashboard.navigateToIndependentFoodsCo();
+        Dashboard.navigateToOrderGuide();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToOrderGuide(),"navigation error");
+        itemName = Customer.getItemNameFirstRow();
+        searchItemCode = Customer.getItemCodeFirstRow();
+        itemPrice = Customer.getActiveItemPriceFirstRow();
+        Customer.searchItemOnOrderGuide(searchItemCode);
+        Customer.increaseFirstRowQtyCustom(1);
+        softAssert.assertEquals(Customer.getItemPriceOnCheckoutButton(),itemPrice,"The item has not been selected.");
+        Customer.checkoutItems();
+        softAssert.assertTrue(Customer.isReviewOrderTextDisplayed(), "The user is unable to land on the Review Order page.");
+        softAssert.assertEquals(Customer.getItemNameFirstRow(), itemName, "The item selected by the user is different from what is shown on the order review page.");
+
+        Dashboard.navigateToDrafts();
+        softAssert.assertTrue(Drafts.isUserNavigatedToDrafts(),"navigation error");
+        referenceNum = Drafts.getReferenceNum();
+        softAssert.assertTrue(Drafts.isLastDraftDisplayed(String.valueOf(itemPrice)),"draft creating error");
+
+        referenceNum = Drafts.getReferenceNum();
+        Drafts.clickTrashIcon();
+        softAssert.assertTrue(Drafts.isDraftsDeleteTextDisplayed(),"delete draft pop up not display");
+        softAssert.assertTrue(Drafts.isDraftDeleteConfirmationDisplayed(draftConfirmation),"draft confirmation not display");
+        Drafts.clickYesButton();
+        softAssert.assertFalse(Drafts.isReferenceNumberDisplayed(referenceNum),"delete draft not successfully ");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
