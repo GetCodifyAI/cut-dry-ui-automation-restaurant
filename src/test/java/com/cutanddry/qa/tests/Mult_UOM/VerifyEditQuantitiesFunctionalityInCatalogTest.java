43c76c06a93142c5dcd285228c74f3a77c1545ad
virangabandara <viranga@cutanddry.com>
2025-02-10 09:29:33 +0530

    Add DOT-TC-1067 , DOT-TC-1052

---
+++ src/test/java/com/cutanddry/qa/tests/Mult_UOM/VerifyEditQuantitiesFunctionalityInCatalogTest.java
@@ -0,0 +1,69 @@
+package com.cutanddry.qa.tests.Mult_UOM;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.*;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyEditQuantitiesFunctionalityInCatalogTest extends TestBase {
+    static User user;
+    String searchItemName = "Organic Bananas - 2 LB";
+    String searchItemCde = "00529";
+    String uomDropDownOption = "Multiple Units";
+    String uom1 = "1";
+    String uom2 = "2";
+    static double itemPriceUOM1 ,itemPriceUOM2;
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-1067")
+    public void VerifyEditQuantitiesFunctionalityInCatalog() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToIndependentFoodsCo();
+        Dashboard.navigateToOrderGuide();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToOrderGuide(),"navigation error");
+        Customer.goToCatalog();
+        Customer.searchItemOnCatalog(searchItemCde);
+        softAssert.assertTrue(Customer.getFirstElementFrmSearchResults().contains(searchItemName.toLowerCase()), "item not found");
+        Catalog.ClickOnMultiUomDropDown(searchItemName);
+        Catalog.ClickOnMultiUomDropDownOption(uomDropDownOption);
+        softAssert.assertTrue(Customer.isProductDetailsDisplayed(),"The user is unable to land on the Product Details page.");
+        itemPriceUOM1 = Catalog.getPDPPriceUOM(uom1);
+        itemPriceUOM2 = Catalog.getPDPPriceUOM(uom2);
+        Catalog.clickAddToCartPlusIcon(1, uom1);
+        Catalog.clickAddToCartPlusIcon(1, uom2);
+        softAssert.assertEquals(Math.round(Customer.getItemPriceOnCheckoutButtonViaPDP() * 100.0) / 100.0,
+                ((Math.round(itemPriceUOM1 * 100.0) / 100.0)+(Math.round(itemPriceUOM2 * 100.0) / 100.0)), "The item has not been selected.");
+        Customer.clickOnBackBtnInEditOrderGuide();
+        softAssert.assertTrue(Catalog.isEditQuantitiesButtonDisplayed(searchItemName),"edit quantities button display error");
+        Catalog.ClickOnEditQuantities(searchItemName);
+        softAssert.assertTrue(Customer.isProductDetailsDisplayed(),"The user is unable to land on the Product Details page.");
+        Catalog.clickAddToCartMinusIcon(1, uom1);
+        Catalog.clickAddToCartMinusIcon(1, uom1);
+        softAssert.assertEquals(Math.round(Customer.getItemPriceOnCheckoutButtonViaPDP() * 100.0) / 100.0, 0.00, "The item price is not $0.00.");
+        Catalog.clickBackToCatalog();
+        softAssert.assertTrue(Catalog.isAddToCartButtonDisplayed(),"Add to Cart button not displayed");
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
