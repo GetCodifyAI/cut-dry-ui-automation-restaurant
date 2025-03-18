2014d2aabb2f87119dc029a0e4bb3ccba1a37139
virangabandara <viranga@cutanddry.com>
2025-03-18 07:57:48 +0530

    add VerifyThSelectMultipleUOMJustInTimeItemAndSubmissionTest

---
+++ src/test/java/com/cutanddry/qa/tests/Mult_UOM/VerifyThSelectMultipleUOMJustInTimeItemAndSubmissionTest.java
@@ -0,0 +1,81 @@
+package com.cutanddry.qa.tests.Mult_UOM;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.*;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyThSelectMultipleUOMJustInTimeItemAndSubmissionTest extends TestBase {
+    static User user;
+    String searchItemName = "Beef New York 14OZ Cca C/C";
+    String searchItemCde = "10088";
+    String OperatorName = "katdmejia2@gmail.com";
+    String uomDropDownOption = "Multiple Units";
+    String uom1 = "1";
+    String uom2 = "2";
+    static double itemPriceUOM1 ,itemPriceUOM2,totalPDPItemPrice;
+    String orderId,totalItemQuantityReviewOrder;
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-1039")
+    public void VerifyThSelectMultipleUOMJustInTimeItemAndSubmission() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Login.navigateToLoginAs();
+        Login.loginAsAdminWL(OperatorName);
+        restaurantUI.switchToNewTab();
+
+        Customer.clickOnPlaceOrder();
+        softAssert.assertTrue(Dashboard.isUserNavigatedToOrderGuide(),"navigation error");
+        Customer.goToCatalog();
+        Customer.searchItemOnCatalog(searchItemCde);
+        softAssert.assertTrue(Customer.getFirstElementFrmSearchResults().contains(searchItemName.toLowerCase()), "item not found");
+        Catalog.ClickOnMultiUomDropDown(searchItemName);
+        Catalog.ClickOnMultiUomDropDownOption(uomDropDownOption);
+        softAssert.assertTrue(Customer.isProductDetailsDisplayed(),"The user is unable to land on the Product Details page.");
+        itemPriceUOM1 = Catalog.getPDPPriceUOM(uom1);
+        itemPriceUOM2 = Catalog.getPDPPriceUOM(uom2);
+        Catalog.clickAddToCartPlusIcon(1, uom1);
+        Catalog.clickAddToCartPlusIcon(1, uom2);
+        totalPDPItemPrice = Customer.getItemPriceOnCheckoutButtonViaPDP();
+        softAssert.assertEquals(Math.round(totalPDPItemPrice * 10.0) / 10.0,
+                (Math.round((itemPriceUOM1 + itemPriceUOM2) * 10.0) / 10.0), "The item has not been selected.");
+        Customer.clickCheckOutPDP();
+        softAssert.assertTrue(Customer.isReviewOrderTextDisplayed(), "The user is unable to land on the Review Order page.");
+        totalItemQuantityReviewOrder = Catalog.getTotalQuantityInReviewOrder();
+        Customer.submitOrder();
+        softAssert.assertTrue(Customer.isThankingForOrderPopupDisplayed(), "The order was not completed successfully.");
+        orderId = Customer.getSuccessOrderId();
+        Customer.clickClose();
+
+        History.goToHistory();
+        softAssert.assertTrue(History.isUserNavigatedToHistory(),"History navigation error");
+        History.searchOrderID(orderId);
+        softAssert.assertTrue(History.checkIfSearchedElementVisible(orderId), "Order ID not found in the table.");
+        History.clickOnFirstItemOfOrderHistory();
+        softAssert.assertEquals(Catalog.getTotalQuantityInOrder(),totalItemQuantityReviewOrder,"order quantity not successfully submitted");
+        softAssert.assertEquals(Catalog.getTotalPriceInOrder(),totalPDPItemPrice, 0.01,"order not successfully submitted");
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
