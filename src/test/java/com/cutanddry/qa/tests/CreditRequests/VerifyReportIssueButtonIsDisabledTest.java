d0492b300dc7daf18cb757b90e73db2c3973d7a1
virangabandara <viranga@cutanddry.com>
2025-04-03 08:53:29 +0530

    add VerifyReportIssueButtonIsDisabledTest

---
+++ src/test/java/com/cutanddry/qa/tests/CreditRequests/VerifyReportIssueButtonIsDisabledTest.java
@@ -0,0 +1,77 @@
+package com.cutanddry.qa.tests.CreditRequests;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.*;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyReportIssueButtonIsDisabledTest extends TestBase {
+    static User user;
+    String itemName,orderId;
+    String distributor = "Independent Foods Co";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+    @Test(groups = "DOT-TC-489")
+    public void VerifyReportIssueButtonIsDisabled() throws InterruptedException{
+
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        Assert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+
+        Login.navigateToInternalTools();
+        InternalTools.navigateToConfigureSupplier();
+        InternalTools.navigateToEditDetails(distributor);
+        InternalTools.navigateToOrderingSettingsTab();
+        InternalTools.enableReportIssueOptionToggle(false);
+        InternalTools.clickSave();
+        softAssert.assertTrue(InternalTools.isSuccessPopUpDisplayed(),"change not save");
+        InternalTools.clickOkOnSuccessBtn();
+
+        Login.navigateToOperator();
+        Dashboard.navigateToIndependentFoodsCo();
+        Dashboard.navigateToOrderGuide();
+        Assert.assertTrue(Dashboard.isUserNavigatedToOrderGuide(),"navigation error");
+        itemName = Customer.getItemNameFirstRow();
+        Customer.increaseFirstRowQtyInClassic(1);
+        Customer.checkoutItems();
+        softAssert.assertEquals(Customer.getItemNameFirstRow(),itemName,"item mismatch");
+        Customer.submitOrder();
+        softAssert.assertTrue(Customer.isThankingForOrderPopupDisplayed(), "The order was not completed successfully.");
+        orderId = Customer.getSuccessOrderId();
+        Customer.clickClose();
+
+        History.goToHistory();
+        Assert.assertTrue(History.isUserNavigatedToHistory(),"History navigation error");
+        History.searchOrderID(orderId);
+        softAssert.assertTrue(History.checkIfSearchedElementVisible(orderId), "Order ID not found in the table.");
+        History.clickOnFirstItemOfOrderHistory();
+        softAssert.assertFalse(CreditRequests.isReportIssueButtonDisplayed(),"Credit report issue button not display");
+
+        Login.navigateToInternalTools();
+        InternalTools.navigateToConfigureSupplier();
+        InternalTools.navigateToEditDetails(distributor);
+        InternalTools.navigateToOrderingSettingsTab();
+        InternalTools.enableReportIssueOptionToggle(false);
+        InternalTools.disableHideCheckInOptionToggle(false);
+        InternalTools.clickSave();
+        softAssert.assertTrue(InternalTools.isSuccessPopUpDisplayed(),"change not save");
+        InternalTools.clickOkOnSuccessBtn();
+        softAssert.assertAll();
+    }
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
