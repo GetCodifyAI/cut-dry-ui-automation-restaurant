3ba083be8de7eac5f3fed2f518715d079e2675b2
MRathnayaka <mrathnayaka@mitrai.com>
2025-08-06 09:04:46 +0530

    Staging Test dot-9807

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
     public static String SLACK_WEBHOOK = System.getenv("STAGING_SLACK_WEBHOOK");
     public static String PUBLIC_PAY_URL = "https://app-uat.staging.cutanddry.com/public-pay/";
     public static String SAVAL_FOOD_URL = "https://savalfoodservice-uat.staging.cutanddry.com/log-in?redirect_on_login=%2F";
-    public static final String TEST_STAG ="dot-894-1-new";
+    public static final String TEST_STAG ="dot-9807";
 
 
     //slackToken=***
