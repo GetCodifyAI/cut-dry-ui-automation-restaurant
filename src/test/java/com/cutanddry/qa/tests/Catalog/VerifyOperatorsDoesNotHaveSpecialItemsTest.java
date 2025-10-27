43564f320cccd6cf88cf1faa94de521aa60ea344
virangabandara <viranga@cutanddry.com>
2025-10-27 08:45:49 +0530

    add DOT-TC-1411 , DOT-TC-1427

---
+++ src/test/java/com/cutanddry/qa/tests/Catalog/VerifyOperatorsDoesNotHaveSpecialItemsTest.java
@@ -0,0 +1,52 @@
+package com.cutanddry.qa.tests.Catalog;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Customer;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Order;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyOperatorsDoesNotHaveSpecialItemsTest extends TestBase {
+    static User user;
+    String searchItemCode = "20475";
+    String itemName = "Mascarpone Domestic 00016";
+
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test(groups = "DOT-TC-1427")
+    public void VerifyOperatorsDoesNotHaveSpecialItems() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.loginAsRestaurant(user.getEmailOrMobile(), user.getPassword());
+        Dashboard.isUserNavigatedToDashboard();
+        Assert.assertTrue(Dashboard.isUserNavigatedToDashboard(),"login error");
+        Dashboard.navigateToIndependentFoodsCo();
+        Dashboard.navigateToOrderGuide();
+        Assert.assertTrue(Dashboard.isUserNavigatedToOrderGuide(),"navigation error");
+
+        Customer.goToCatalog();
+        Customer.searchItemOnCatalog(searchItemCode);
+        softAssert.assertFalse(Customer.getFirstElementFrmSearchResults().contains(itemName.toLowerCase()), "item not found");
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+
+}
